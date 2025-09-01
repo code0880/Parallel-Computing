20d8fbb5ac0398c200b607465e3ad9d6015c782d
VINYAS <vinyas.22cs183@sode-edu.in>
2025-09-01 10:11:58 +0530

    MPI_SEND and MPI_RECV

--- Prog5.c
+++ Prog5.c
@@ -1,58 +1,24 @@
-// OpenMP program to transform array elements: a[i] = 3 * a[i] + 2
-// Using #pragma omp parallel for with dynamic and static scheduling
-
 #include <stdio.h>
-#include <omp.h>
+#include <mpi.h>
 
-#define SIZE 20
+int main(int argc, char *argv[]) {
+    int rank, data_send, data_recv;
 
-int main() {
-    int a[SIZE];
-    int i;
-    
-    // Initialize array
-    for (i = 0; i < SIZE; i++) {
-        a[i] = i + 1;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    data_send = rank;
+
+    if (rank == 0) {
+        MPI_Send(&data_send, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&data_recv, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    } else if (rank == 1) {
+        MPI_Send(&data_send, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Recv(&data_recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
-    
-    printf("Original array:\n");
-    for (i = 0; i < SIZE; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n\n");
-    
-    // Transform array using dynamic scheduling (chunk size 2)
-    printf("Using dynamic scheduling (chunk size 2):\n");
-    #pragma omp parallel for schedule(dynamic, 2)
-    for (i = 0; i < SIZE; i++) {
-        a[i] = 3 * a[i] + 2;
-        printf("Thread %d processed element %d\n", omp_get_thread_num(), i);
-    }
-    
-    printf("\nArray after dynamic scheduling:\n");
-    for (i = 0; i < SIZE; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n\n");
-    
-    // Reset array for static scheduling demonstration
-    for (i = 0; i < SIZE; i++) {
-        a[i] = i + 1;
-    }
-    
-    // Transform array using static scheduling (chunk size 2)
-    printf("Using static scheduling (chunk size 2):\n");
-    #pragma omp parallel for schedule(static, 2)
-    for (i = 0; i < SIZE; i++) {
-        a[i] = 3 * a[i] + 2;
-        printf("Thread %d processed element %d\n", omp_get_thread_num(), i);
-    }
-    
-    printf("\nArray after static scheduling:\n");
-    for (i = 0; i < SIZE; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("\n");
-    
+
+    printf("Process %d received %d\n", rank, data_recv);
+
+    MPI_Finalize();
     return 0;
 }
